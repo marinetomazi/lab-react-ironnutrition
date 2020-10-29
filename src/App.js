d30451387248bcc837b305088c9fd921d7c6e959
Pierre-Alderic Boegler <pa.boegler@gmail.com>
2020-10-29 20:46:01 +0100

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -11,29 +11,69 @@ class App extends React.Component {
 
   state = {
     foodList: foods.slice(0, 10),
+    search: '',
 
-}
+  }
 
-fooding = (food) => {
-this.setState({
- foodList :[...this.state.foodList, food]
-})
-}
+  fooding = (food) => {
+    this.setState({
+      foodList: [...this.state.foodList, food]
+    })
+  }
 
- render(){
-  return (
-    <div className="App">
-      <AddNewFoods addFood = {this.fooding} />
-      {this.state.foodList.map((food, index) => {
-                    console.log(food)
-                    return (
-                        <FoodBox {...food} Key ={index}/>
-                    )
-                })}
-    </div>
-  );
- }
- 
+  searchBar = (event) => {
+    const lookUpWord = event.target.value
+    const searchResult = []
+
+
+    // SEARCH RESET
+    // if event,target.value.length === 0
+
+    this.state.foodList.forEach(el => {
+      console.log(el.name.includes(lookUpWord))
+      if (el.name.includes(lookUpWord)) {
+        searchResult.push(el)
+      }
+    })
+    console.log("searchResult===", searchResult)
+    this.setState({
+      search: lookUpWord,
+      foodList: searchResult
+    })
+
+
+
+    // this.state.foodList.slice().filter(food => {
+    //   //food.name doit inclure les lettres dans lookUpWord
+    //   if (food.name.includes(lookUpWord)) {
+    //     return this.setState({
+    //       foodList: 
+    //     })
+    //   }
+    // })
+
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <form>
+          <input type="text" name="search" value={this.state.search} onChange={this.searchBar} placeholder="search" />
+        </form>
+
+        <AddNewFoods addFood={this.fooding} />
+        {this.state.foodList.map((food, index) => {
+          {/* console.log(food) */ }
+          return (
+            <FoodBox {...food} Key={index} />
+          )
+        })}
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/Foodbox/Foodbox.js
+++ src/Foodbox/Foodbox.js
@@ -2,40 +2,40 @@ import React from 'react';
 import foods from '../foods.json';
 
 class FoodBox extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="box">
-          <article className="media">
-            <div className="media-left">
-              <figure className="image is-64x64">
-                <img src={this.props.image} alt="" />
-              </figure>
-            </div>
-            <div className="media-content">
-              <div className="content">
-                <p>
-                  <strong>{this.props.name}</strong> <br />
-                  <small>{this.props.calories}</small>
-                </p>
-              </div>
-            </div>
-            <div className="media-right">
-              <div className="field has-addons">
-                <div className="control">
-                  <input className="input" type="number" value="1" />
+    render() {
+        return (
+            <div>
+                <div className="box" key={this.props.key}>
+                    <article className="media">
+                        <div className="media-left">
+                            <figure className="image is-64x64">
+                                <img src={this.props.image} alt="" />
+                            </figure>
+                        </div>
+                        <div className="media-content">
+                            <div className="content">
+                                <p>
+                                    <strong>{this.props.name}</strong> <br />
+                                    <small>{this.props.calories}</small>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="media-right">
+                            <div className="field has-addons">
+                                <div className="control">
+                                    <input className="input" type="number" value="1" />
+                                </div>
+                                <div className="control">
+                                    <button className="button is-info">+</button>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
                 </div>
-                <div className="control">
-                  <button className="button is-info">+</button>
-                </div>
-              </div>
+
             </div>
-          </article>
-        </div>
-        
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default FoodBox;
